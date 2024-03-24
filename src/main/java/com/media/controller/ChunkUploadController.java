0a960db6f91bc60fabee490b598f38418adad0a6
jungdaehun1024 <pooh99102408@gmail.com>
2024-03-25 01:36:46 +0900

    파일분할 업로드 완료 + 주석 24_03_25

--- src/main/java/com/media/controller/ChunkUploadController.java
+++ src/main/java/com/media/controller/ChunkUploadController.java
@@ -28,15 +28,19 @@ public class ChunkUploadController {
 		return "chunk";
 	}
 	
+	//클라이언트로부터 받는 HTTP Body에서 이름이 일치하는 값을 추출해서 매핑 
 	@ResponseBody
 	@PostMapping("/chunk/upload")
 	public ResponseEntity<String> chunkUpload(@RequestPart("chunk")MultipartFile file,
 																	@RequestParam("chunkNumber")int chunkNumber,
 																	@RequestParam("totalChunks")int totalChunks)throws IOException{
 		
-		
+		//chunkUpload가 T인지 F인지 저장 
 		boolean isDone = chunkUploadService.chunkUpload(file, chunkNumber, totalChunks);
+		// true이면 ok(200)과 함께 문자열 보내줌 (청크파일이 끝임을 알려준다.)
 		return isDone ? ResponseEntity.ok("File uploaded Successfully"):
+								//HttpStatus.PARTIAL_CONTENT는 206과 동등한 상태코드를 나타냄 
+								// .build()로 최종적으로 ResponseEntity객체를 만들어야 클라이언트에게 실제로 응답이 전송됨
 								ResponseEntity.status(HttpStatus.PARTIAL_CONTENT).build();
 		
 	}
